ade4d22d467e7a371d37cb089232c6380f782d9f
Mickey253 <jacob_l._miller@hotmail.com>
2023-02-17 09:48:08 -0700

    did puzzles

--- 2016/22/code.py
+++ 2016/22/code.py
@@ -2,12 +2,34 @@
 # Author = ?
 # Date = December 2022
 
+import re
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+    input = [[int(n) for n in re.findall(r'\d+', d.strip())] for d in input_file.readlines()][2:]
+
+class Node():
+    def __init__(self,x,y,size,used,avail,per):
+        self.x = x
+        self.y = y 
+        self.size = size 
+        self.used = used 
+        self.avail = avail 
+        self.per = per
+        if per == 0:
+            print("I am empty")
+
+nodes = [Node(*d) for d in input]
+n = len(nodes)
+
+p1 = 0
+for i in range(n):
+    for j in range(n):
+        if i == j: continue
+        A,B = nodes[i], nodes[j]
+        if A.used > 0 and A.used <= B.avail: 
+            p1 += 1
 
 
-
-print("Part One : "+ str(None))
+print("Part One : "+ str(p1))
 
 
 
