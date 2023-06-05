32e06dbfaf8e465c37af0ac681d1823f34c9a299
Jacob Lee Miller <jacobmiller1@cs.arizona.edu>
2023-06-05 08:56:10 -0700

    update

--- 2017/3/code.py
+++ 2017/3/code.py
@@ -3,12 +3,57 @@
 # Date = December 2018
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+    input = int(input_file.read())
+
+def manhattan(x1,y1,x2,y2):
+    return abs(x2-x1) + abs(y2-y1) 
+
+import math
+def spiral(n):
+        k = math.ceil((math.sqrt(n)-1)/2)
+        t=2*k+1
+        m=t**2 
+        t=t-1
+        if n >= m-t:  return k-(m-n),-k        
+        else: m=m-t
+        if n >= m-t:  return -k,-k+(m-n) 
+        else: m=m-t
+        if n >= m-t: return -k+(m-n),k 
+        else: return k,k-(m-n-t)
+
+p = spiral(input)
+
+p1 = manhattan(0,0,*p)
+
+print("Part One : "+ str(p1))
+
+def calc(cur,grid):
+    dirs = [1,1+1j,1j,-1+1j,-1,-1-1j,-1j,1-1j]
+    val = 0
+    for d in dirs:
+        if cur+d in grid:
+            val += grid[cur+d]
+    return val
+
+
+def spiral2(n):
+    grid = {0+0j: 1}
+    dirs = [1,1j,-1,-1j]
+    i,d = 1,0 
+    num = 1
+    cur = 0+0j
+    while i < n:
+        for _ in range(2):
+            for j in range(num):
+                cur = cur + dirs[d]
+                grid[cur] = calc(cur,grid)
+                i += 1
+                if grid[cur] > n: return grid[cur]
+            d = (d+1)%4
+        num += 1
+
+p2 = spiral2(input)
 
 
 
-print("Part One : "+ str(None))
-
-
-
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(p2))
--- 2017/9/code.py
+++ 2017/9/code.py
@@ -5,10 +5,47 @@
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
+# input = "{{<a!>},{<a!>},{<a!>},{<ab>}}"
+
+import re
+
+def remove_substring(s):
+    ns = ""
+    i = 0 
+    while i < len(s):
+        if s[i] == "!" and i < len(s)-1:
+            i += 2 
+        else:
+            ns += s[i]
+            i += 1
+    return ns
+
+def remove_between_angle_brackets(string):
+    pattern = r"<.*?>"
+    return re.sub(pattern, "", string)    
+
+ns = remove_substring(input)
+og_len = len(ns)
+
+ns = remove_between_angle_brackets(ns)
+
+depth = 0 
+p1 = 0
+for c in ns:
+    if c == "{":
+        depth += 1
+    elif c == "}":
+        p1 += depth 
+        depth -= 1
 
 
-print("Part One : "+ str(None))
+print("Part One : "+ str(p1))
 
+nns = remove_substring(input)
+pattern = r"<.*?>"
+nns = re.sub(pattern,"!",nns)
 
+num_brackets = nns.count("!")
+p2 = og_len - len(ns) - 2*num_brackets
 
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(p2))
